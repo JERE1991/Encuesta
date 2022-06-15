cc0c9feb87b7ac5db859783b9f55c2b3eabc2981
Jeremias Santana <santanajere@gmail.com>
2022-06-14 23:41:11 -0400

    Implementar Servicio.

---
+++ Encuesta/Servicios/RepositoriodeEncuesta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Encuesta.Models;
+
+namespace Encuesta.Servicios
+{ 
+
+
+    public interface IRepositoriodeEncuesta
+{
+        void FormularlaEncuesta(FormularioEncuesta formularioEncuesta);
+}
+    public class RepositoriodeEncuesta: IRepositoriodeEncuesta
+    {
+       private readonly string connectionString;
+
+        public void FormularlaEncuesta(FormularioEncuesta formularioEncuesta) { 
+        
+        
+        
+        }
+
+
+
+
+
+    }
+}
